a38043feda20cc8a7fd3a28a61ea7145aae5f850
Jason Frerich <jason.frerich@mattermost.com>
2020-10-23 14:59:51 -0500

    wip

--- e2e/cypress/integration/slash_commands/autocomplete_spec.js
+++ e2e/cypress/integration/slash_commands/autocomplete_spec.js
@@ -12,6 +12,8 @@
 /**
  * Note : This test requires the demo plugin tar file under fixtures folder.
  * Download :
+ * NOTE : latest demo plugin has autocomplete tests, but is not yet released.
+ * make dist latest master and copy to ./e2e/cypress/fixtures/com.mattermost.demo-plugin-0.8.0.tar.gz
  * https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz
  * Copy to : ./e2e/cypress/fixtures/com.mattermost.demo-plugin-0.8.0.tar.gz
 
@@ -44,7 +46,8 @@ describe('Integrations', () => {
         // cy.apiInstallPluginFromUrl(jiraURL);
         // cy.apiInstallPluginFromUrl(agendaURL);
 
-        // cy.apiUploadPlugin('filename');
+        // const demoFile = 'com.mattermost.demo-plugin-0.8.0.tar.gz';
+        // cy.apiUploadPlugin(demoFile);
     });
 
     after(() => {
@@ -53,7 +56,124 @@ describe('Integrations', () => {
         // cy.apiRemovePluginById(pluginIdAgenda);
     });
 
-    it('T2834 Slash command help stays visible for system slash command', () => {
+    it('DONE - T2829 Test an example of plugin that uses sub commands', () => {
+        // # Post a slash command with trailing space
+        cy.get('#post_textbox').clear().type('/jira ');
+
+        // * Verify suggestion list is visible and includes 'info'
+        cy.get('#suggestionList').findByText('info').scrollIntoView().should('be.visible');
+
+        // # Narrow down list to show info only suggestion
+        cy.get('#post_textbox').type('inf');
+
+        // * Verify suggestion list is visible with only 1 child
+        cy.get('#suggestionList').should('be.visible').children().should('have.length', 1);
+
+        // * Verify list is refined to only the info sub command
+        cy.get('#suggestionList').findByText('info').should('be.visible');
+
+        // # click the info subcommand and hit enter
+        cy.get('#suggestionList').findByText('info').should('be.visible').click();
+        cy.get('#post_textbox').type('{enter}');
+
+        // * Verify message is sent and (only visible to you)
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).
+                should('contain.text', '(Only visible to you)').
+                should('contain.text', 'is not a valid Mattermost SITEURL.');
+        });
+    });
+
+    it('DONE - T2830 Test an example of a plugin using static list', () => {
+        // # Post a slash command with trailing space
+        cy.get('#post_textbox').clear().type('/jira ');
+
+        // * Verify suggestion includes instance subcommand and is visible
+        cy.get('#suggestionList').should('contain.text', 'instance').scrollIntoView().should('be.visible');
+
+        // # Narrow down list to show info only suggestion
+        cy.get('#post_textbox').type('i');
+
+        // * Verify suggestion list is visible with at three children (issue, instance, info)
+        cy.get('#suggestionList').should('be.visible').children().
+            should('contain.text', 'issue').
+            should('contain.text', 'instance').
+            should('contain.text', 'info');
+
+        // # Clear test and post a slash command with trailing space
+        cy.get('#post_textbox').clear().type('/jira instance settings ');
+
+        // * Verify suggestion notifications is visible
+        cy.get('#suggestionList').should('contain.text', 'notifications').scrollIntoView().should('be.visible');
+
+        // # Clear test and Post a slash command with trailing space
+        cy.get('#post_textbox').type('notifications ');
+
+        // * Verify notifications suggestion is visible and lists on/off options
+        cy.get('#suggestionList').should('be.visible').children().
+            should('contain.text', 'on').
+            should('contain.text', 'off');
+
+        // # down arrow to highlight the second suggestion
+        // # enter to push to send command to post textbox
+        cy.get('#post_textbox').type('{downarrow}{downarrow}{enter}');
+
+        // # Send the command
+        cy.get('#post_textbox').type('{enter}');
+
+        // * Verify message is sent and (only visible to you)
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).
+                should('contain.text', '(Only visible to you)').
+                should('contain.text', 'is not a valid Mattermost SITEURL.');
+        });
+    });
+
+    it('DONE - T2831 Test an example of plugin using dynamic list', () => {
+        // # Post a slash command with trailing space
+        cy.get('#post_textbox').clear().type('/autocomplete_test dynamic-arg ');
+
+        // * Verify suggestion list is visible with at three children (issue, instance, info)
+        cy.get('#suggestionList').should('be.visible').children().
+            should('contain.text', 'suggestion 1 (hint)').
+            should('contain.text', 'suggestion 2 (hint)');
+
+        // # down arrow to highlight the second suggestion
+        // # enter to push to send command to post textbox
+        cy.get('#post_textbox').type('{downarrow}{downarrow}{enter}');
+
+        // # Send the command
+        cy.get('#post_textbox').type('{enter}');
+
+        // * Verify correct message is sent,  (only visible to you)
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).
+                should('contain.text', '(Only visible to you)').
+                should('contain.text', 'Executed command: /autocomplete_test dynamic-arg suggestion 2');
+        });
+    });
+
+    it.only('T2832 Use a slash command that omits the optional argument', () => {
+        // # Post a slash command that omits the optional argument
+        cy.get('#post_textbox').clear().type('/autocomplete_test optional-arg {enter}');
+
+        // * Verify item is sent and is (only visible to you)
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).contains('Executed command: /autocomplete_test optional-arg');
+        });
+    });
+
+    it.only('T2833 Use a slash command that accepts an optional argument', () => {
+        // # Post a slash command that accepts an optional argument
+        cy.get('#post_textbox').clear().type('/autocomplete_test optional-arg --name1 testarg {enter}');
+
+        // * Verify item is sent and is (only visible to you)
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).contains('Executed command: /autocomplete_test optional-arg --name1 testarg');
+        });
+    });
+
+    it('DONE - T2834 Slash command help stays visible for system slash command', () => {
         // # Post a slash command without trailing space
         cy.get('#post_textbox').type('/rename');
 
@@ -79,101 +199,11 @@ describe('Integrations', () => {
         cy.get('.slash-command__desc').contains('[text]').should('be.visible');
     });
 
-    it('T2835 Slash command help stays visible for plugin', () => {
-        // # Post a slash command without trailing space
+    it('DONE - T2835 Slash command help stays visible for plugin', () => {
+        // # Post a slash command with trailing space
         cy.get('#post_textbox').clear().type('/jira ');
 
         // * Verify suggestion list is visible with 11 children
         cy.get('#suggestionList').should('be.visible').children().should('have.length', 11);
-    });
-
-    it('T2829 Test an example of plugin that uses sub commands', () => {
-        // # Post a slash command with trailing space
-        cy.get('#post_textbox').clear().type('/jira ');
-
-        // * Verify suggestion list is visible
-        cy.get('#suggestionList').findByText('info').scrollIntoView().should('be.visible');
-
-        // # Narrow down list to show info only suggestion
-        cy.get('#post_textbox').type('inf');
-
-        // * Verify suggestion list is visible with only 1 child
-        cy.get('#suggestionList').should('be.visible').children().should('have.length', 1);
-
-        // * Verify list is refined to only the info sub command
-        cy.get('#suggestionList').findByText('info').should('be.visible');
-
-        // # click the info subcommand and hit enter
-        cy.get('#suggestionList').findByText('info').should('be.visible').click();
-        cy.get('#post_textbox').type('{enter}');
-
-        // * Verify message is sent and (only visible to you)
-        cy.getLastPostId().then((postId) => {
-            cy.get(`#post_${postId}`).
-                should('contain.text', '(Only visible to you)').
-                should('contain.text', 'is not a valid Mattermost SITEURL.');
-        });
-    });
-
-    it.only('T2830 Test an example of plugin using static list', () => {
-        // # Post a slash command with trailing space
-        cy.get('#post_textbox').clear().type('/jira ');
-
-        // * Verify suggestion list is visible
-        cy.get('#suggestionList').should('contain.text', 'instance').scrollIntoView().should('be.visible');
-
-        // # Narrow down list to show info only suggestion
-        cy.get('#post_textbox').type('i');
-
-        // * Verify suggestion list is visible with at three children (issue, instance, info)
-        cy.get('#suggestionList').should('be.visible').children().
-            should('contain.text', 'issue').
-            should('contain.text', 'instance').
-            should('contain.text', 'info');
-
-        // # Clear test and Post a slash command with trailing space
-        cy.get('#post_textbox').clear().type('/jira instance settings ');
-
-        // * Verify suggestion notifications is visible
-        cy.get('#suggestionList').should('contain.text', 'notifications').scrollIntoView().should('be.visible');
-
-        // # Clear test and Post a slash command with trailing space
-        cy.get('#post_textbox').type('notifications ');
-
-        // * Verify suggestion list is visible with at three children (issue, instance, info)
-        cy.get('#suggestionList').should('be.visible').children().
-            should('contain.text', 'on').
-            should('contain.text', 'off');
-
-        cy.get('#suggestionList').type('{upArrow}');
-        cy.get('#suggestionList').type('{downArrow}').click();
-
-        // // * Verify list is refined to settings and subscribe commands
-        // cy.get('#suggestionList').findByText('settings').should('be.visible');
-        // cy.get('#suggestionList').findByText('subscribe').should('be.visible');
-        //
-        // // # Narrow down list to show info only suggestion
-        // cy.get('#post_textbox').type('e');
-        //
-        // // * Verify suggestion list is visible with only 1 child
-        // cy.get('#suggestionList').should('be.visible').children().should('have.length', 1);
-        //
-        // // * Verify list is refined to settings sub command
-        // cy.get('#suggestionList').findByText('settings').should('be.visible');
-        //
-        // // # click the info subcommand and hit enter
-        // cy.get('#suggestionList').findByText('settings').should('be.visible').click();
-        // cy.get('#post_textbox').type('{enter}');
-        //
-        // // # click the info subcommand and hit enter
-        // cy.get('#suggestionList').findByText('info').should('be.visible').click();
-        // cy.get('#post_textbox').type('{enter}');
-        //
-        // // * Verify message is sent and (only visible to you)
-        // cy.getLastPostId().then((postId) => {
-        //     cy.get(`#post_${postId}`).
-        //         should('contain.text', '(Only visible to you)').
-        //         should('contain.text', 'is not a valid Mattermost SITEURL.');
-        // });
     });
 });
